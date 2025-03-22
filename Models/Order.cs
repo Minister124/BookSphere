7bc24611da9b386620b112aac676f4cfcb05cdff
Minister124 <kushaltmg321@gmail.com>
2025-03-22 09:17:20 +0545

    All Model Files

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,8 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookSphere.Models;
 
 public class Order
 {
+        [Key]
+        public Guid Id {get; set;}
 
+        
 }
