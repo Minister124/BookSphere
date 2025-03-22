0dd2512033d4c01ecb09170516966ce9c19f5848
Minister124 <kushaltmg321@gmail.com>
2025-03-22 10:28:20 +0545

    Model Review

---
+++ Models/Review.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookSphere.Models;
+
+public class Review
+{
+        [Key]
+        public Guid Id {get; set;}
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating {get; set;}
+
+        [StringLength(1000)]
+        public string Comment {get; set;}
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime ReviewDate {get; set;} = DateTime.UtcNow
+}
