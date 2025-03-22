6811581410bd2e261d231baabf8fc7e7be2ca806
Minister124 <kushaltmg321@gmail.com>
2025-03-22 09:13:14 +0545

    Models - User

---
+++ Models/Order.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace BookSphere.Models;
+
+public class Order
+{
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookSphere.Models;
+
+public class User
+{
+        [Key]
+        public Guid Id {get; set;}
+
+        [Required]
+        [StringLength(100)]
+        public string FullName {get; set;}
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string EmailAddress {get; set;}
+
+        [Required]
+        [StringLength(100)]
+        public string PasswordHash {get; set;}
+
+        [Required]
+        [Phone]
+        [StringLength(10)]
+        public string Phone {get; set;}
+
+        [StringLength(100)]
+        public string Address {get; set;}
+
+        [DataType(DataType.DateTime)]
+        public DateTime RegisteredDate {get; set;} = DateTime.UtcNow;
+
+        [Required]
+        [StringLength(20)]
+        public string Role {get; set;}
+
+        [StringLength(50)]
+        public string MembershipId {get; set;}
+
+        public int SuccessfulOrder {get; set;}
+        public bool HasStackableDiscount {get; set;}
+}
