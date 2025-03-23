ac0823d789930e46bc5593a612e909aa0d050bf2
Minister124 <kushaltmg321@gmail.com>
2025-03-23 12:27:35 +0545

    Jwt Authentication

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using BookSphere.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,63 @@ builder.Services.AddDbContext<BookSphereDbContext>(options =>
         )
 );
 
+builder.Services.AddAuthentication(options => 
+{
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                )
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+                OnMessageReceived = context =>
+                {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/booksphere"))
+                        {
+                                context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                }
+        };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+        options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+        options.AddPolicy("RequireStaffRole", policy => policy.RequireRole("Staff, Admin"));
+        options.AddPolicy("RequireMemberRole", policy => policy.RequireRole("Member"));
+});
+
+builder.Services.AddCors(options =>
+{
+        options.AddPolicy("AllowReactApp", policy => 
+        {
+                policy.WithOrigins(builder.Configuration["AllowedOrigins"]!.Split(';'))
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+});
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
@@ -19,5 +79,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowReactApp");
 
 app.Run();
