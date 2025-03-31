50f9fe41d9bb42765d60d934b5e2ab111f48df78
Minister124 <kushaltmg321@gmail.com>
2025-03-31 11:36:27 +0545

    Book Interface

--- IServices/IBookServices.cs
+++ IServices/IBookServices.cs
@@ -1,8 +1,22 @@
 using System;
+using BookSphere.DTOs;
 
 namespace BookSphere.IServices;
 
 public interface IBookServices
 {
+            Task<PaginatedResponse<BookDto>> GetBookAsync(int pageNumber, int pageSize, BookFIlterDto? fIlterDto); //for browsing and filtering books
 
+            Task<BookDetailsDto> GetBookDetailsAsync(Guid bookId); //Get book details
+
+            //Admin CRUD opration
+            Task<BookDto> CreateBookAsync(CreateBookDto create);
+            Task<BookDto> UpdateBookAsync(Guid id, UpdateBookDto update);
+
+            Task<bool> DeleteBookAsync(Guid id);
+
+            //Admin Inventory Management
+            Task<BookDto> UpdateInventoryAsync(Guid bookId, int quantity);
+            Task<BookDto> SetDiscountAsync(Guid bookId, SetDiscountDto discount);
+            Task<bool> IsInStockAsync(Guid bookId, int quantity);
 }
