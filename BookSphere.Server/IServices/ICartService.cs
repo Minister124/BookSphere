29ea968c9b487448da328c03c01d68479b156be2
Minister124 <kushaltmg321@gmail.com>
2025-04-22 09:33:36 +0545

    Cart Service Interface

--- BookSphere.Server/IServices/ICartService.cs
+++ BookSphere.Server/IServices/ICartService.cs
@@ -1,8 +1,25 @@
 using System;
+using BookSphere.DTOs;
 
 namespace BookSphere.IServices;
 
 public interface ICartService
 {
+          //Get user's cart
+          Task<CartDto> GetCartAsync(Guid userId);
 
+          //Add item to cart
+          Task<CartDto> AddToCartAsync(Guid userId, AddToCartDto addToCartDto);
+
+          //Updte cart item quantity
+          Task<CartDto> UpdateCartItemAsync(Guid userId, UpdateCartItemDto updateCartItemDto);
+
+          //Remove item from cart
+          Task<CartDto> RemoveFromCartAsync(Guid userId, Guid cartItemId);
+
+          //Clear cart
+          Task<bool> ClearCartAsync(Guid userId);
+
+          //Check if cart is valid for check(items in stock, etc)
+          Task<bool> ValidateCartAsync(Guid userId);
 }
