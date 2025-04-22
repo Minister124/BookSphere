bea8290d9d6196f1c5b785f17c26dd776b2f4c3d
Minister124 <kushaltmg321@gmail.com>
2025-04-22 15:25:18 +0545

    Mapping

--- BookSphere.Server/Mapping/MappingProfile.cs
+++ BookSphere.Server/Mapping/MappingProfile.cs
@@ -19,5 +19,57 @@ public class MappingProfile : Profile
                               .ForMember(dest => dest.RegisteredDate, opt => opt.MapFrom(src => DateTime.UtcNow))
                               .ForMember(dest => dest.SuccessfulOrder, opt => opt.MapFrom(src => 0))
                               .ForMember(dest => dest.HasStackableDiscount, opt => opt.MapFrom(src => false));
+                    
+                    //Book Mappings
+                    CreateMap<Book, BookDto>()
+                              .ForMember(dest => dest.DiscountedPrice,
+                                        opt => opt.MapFrom(src => src.Price - (src.Price * src.DiscountPercentage / 100)));
+                    CreateMap<Book, BookDetailsDto>()
+                              .ForMember(dest => dest.DiscountedPrice,
+                                        opt => opt.MapFrom(src => src.Price - (src.Price * src.DiscountPercentage / 100)))
+                              .ForMember(dest => dest.AverageRating,
+                                        opt => opt.MapFrom(src => src.Reviews.Count > 0 ? src.Reviews.Average(r => r.Rating) : 0))
+                              .ForMember(dest => dest.ReviewCount,
+                                        opt => opt.MapFrom(src => src.Reviews.Count));
+                    CreateMap<CreateBookDto, Book>();
+                    CreateMap<UpdateBookDto, Book>();
+
+                    //WhiteList Mapppings
+                    CreateMap<WhiteList, WhiteListDto>()
+                              .ForMember(dest => dest.Items,
+                                        opt => opt.MapFrom(src => src.WhiteListItems));
+                    CreateMap<WhiteListItem, WhiteListItemDto>()
+                              .ForMember(dest => dest.Book,
+                                        opt => opt.MapFrom(src => src.Book));
+
+                    //Review Mappings
+                    CreateMap<Review, ReviewDto>()
+                              .ForMember(dest => dest.UserName,
+                                        opt => opt.MapFrom(src => src.User.FullName));
+                    CreateMap<CreateReviewDto, Review>();
+
+                    //Announcement mappings
+                    CreateMap<Announcement, AnnouncementDto>();
+                    CreateMap<CreateAnnouncementDto, Announcement>()
+                              .ForMember(dest => dest.CreatedDate,
+                                        opt => opt.MapFrom(src => DateTime.UtcNow))
+                              .ForMember(dest => dest.IsActive,
+                                        opt => opt.MapFrom(src => true));
+
+                    // Cart mappings
+                    CreateMap<Cart, CartDto>()
+                              .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items))
+                              .ForMember(dest => dest.SubTotal, opt => opt.Ignore()) // Will be calculated in service
+                              .ForMember(dest => dest.DiscountAmount, opt => opt.Ignore()) // Will be calculated in service
+                              .ForMember(dest => dest.Total, opt => opt.Ignore()) // Will be calculated in service
+                              .ForMember(dest => dest.QualifiesForBulkDiscount, opt => opt.Ignore()) // Calculated in service
+                              .ForMember(dest => dest.HasLoyaltyDiscount, OPT => OPT.Ignore()); // Calculated in service
+                    CreateMap<CartItem, CartItemDto>()
+                              .ForMember(dest => dest.Book, opt => opt.MapFrom(src => src.Book))
+                              .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.Book.Price))
+                              .ForMember(dest => dest.DiscountedPrice,
+                                        opt => opt.MapFrom(src => src.Book.Price - (src.Book.Price * src.Book.DiscountPercentage / 100)))
+                              .ForMember(dest => dest.Subtotal,
+                                        opt => opt.MapFrom(src => (src.Book.Price - (src.Book.Price * src.Book.DiscountPercentage / 100)) * src.Quantity));
           }
 }
