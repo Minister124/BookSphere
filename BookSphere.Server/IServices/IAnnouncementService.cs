6930b43644953c0af9c104c73c5f68ae8e69d3f7
Minister124 <kushaltmg321@gmail.com>
2025-04-22 09:51:14 +0545

    WhiteList Service Interface

--- BookSphere.Server/IServices/IAnnouncementService.cs
+++ BookSphere.Server/IServices/IAnnouncementService.cs
@@ -1,8 +1,28 @@
 using System;
+using BookSphere.DTOs;
 
 namespace BookSphere.IServices;
 
 public interface IAnnouncementService
 {
+          //Get all active announcements
+          Task<List<AnnouncementDto>> GetActiveAnnouncementsAsync();
 
+          //Get announcement by Id
+          Task<AnnouncementDto> GetAnnouncementDto(Guid announcementId);
+
+          //Create new announcement
+          Task<AnnouncementDto> CreateAnnouncementAsync(Guid adminId, CreateAnnouncementDto createAnnouncementDto);
+
+          //Update an announcment
+          Task<AnnouncementDto> UpdateAnnouncementAsync(Guid announcementId, CreateAnnouncementDto updateAnnouncementDto);
+
+          // Delete an announcement
+          Task<bool> DeleteAnnouncementAsync(Guid announcementId);
+
+          //Activate/Deactivate an announcment
+          Task<AnnouncementDto> ToggleAnnouncementStatudAsync(Guid announcementId, bool isActive);
+
+          //Get announcements by type
+          Task<List<AnnouncementDto>> GetAnnouncementsByTypeAsync(string type);
 }
--- BookSphere.Server/IServices/IWhiteListService.cs
+++ BookSphere.Server/IServices/IWhiteListService.cs
@@ -1,8 +1,22 @@
 using System;
+using BookSphere.DTOs;
 
 namespace BookSphere.IServices;
 
 public interface IWhiteListService
 {
+          //Get user's whitelist
+          Task<WhiteListDto> GetWhiteLisTAsync(Guid userId);
 
+          //Add book to whitelist
+          Task<WhiteListDto> AddToWhiteListAsync(Guid userId, AddToWhiteListDto addToWhiteListDto);
+
+          //Remove book from whitelist
+          Task<WhiteListDto> RemoveFromWhiteListAsync(Guid userId, Guid bookId);
+
+          //check if book is in whitelist
+          Task<bool> IsInWhiteListAsync(Guid userId, Guid bookId);
+
+          //Move item from whitelist to cart
+          Task<CartDto> MoveTOCartAsync(Guid userId, Guid bookId, int quantity);
 }
