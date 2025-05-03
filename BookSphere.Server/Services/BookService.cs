2becb17dfb78bdffcfb0f2e1cbbe80d87c03ac87
Minister124 <kushaltmg321@gmail.com>
2025-05-03 22:41:24 +0545

    Book Service

--- BookSphere.Server/Services/BookService.cs
+++ BookSphere.Server/Services/BookService.cs
@@ -1,8 +1,59 @@
 using System;
+using BookSphere.Data;
+using BookSphere.DTOs;
+using BookSphere.IServices;
 
 namespace BookSphere.Services;
 
-public class BookService
+public class BookService : IBookServices
 {
+          private readonly BookSphereDbContext _context;
 
+          public BookService(BookSphereDbContext context)
+          {
+                    _context = context;
+          }
+          public Task<BookDto> CreateBookAsync(CreateBookDto create)
+          {
+                    throw new NotImplementedException();
+          }
+
+          public Task<bool> DeleteBookAsync(Guid id)
+          {
+                    throw new NotImplementedException();
+          }
+
+          public Task<PaginatedResponse<BookDto>> GetBookAsync(int pageNumber, int pageSize, BookFIlterDto fIlterDto)
+          {
+                    throw new NotImplementedException();
+          }
+
+          public Task<BookDetailsDto> GetBookDetailsAsync(Guid bookId)
+          {
+                    throw new NotImplementedException();
+          }
+
+          public async Task<bool> IsInStockAsync(Guid bookId, int quantity)
+          {
+                    var book = await _context.Books.FindAsync(bookId);
+
+                    if(book == null) throw new KeyNotFoundException("Book Not Found");
+
+                    return book.StockQuantity >= quantity;
+          }
+
+          public Task<BookDto> SetDiscountAsync(Guid bookId, SetDiscountDto discount)
+          {
+                    throw new NotImplementedException();
+          }
+
+          public Task<BookDto> UpdateBookAsync(Guid id, UpdateBookDto update)
+          {
+                    throw new NotImplementedException();
+          }
+
+          public Task<BookDto> UpdateInventoryAsync(Guid bookId, int quantity)
+          {
+                    throw new NotImplementedException();
+          }
 }
